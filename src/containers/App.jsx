e50ed5f5998f9590c76da00b0056f2cd8debef16
Jesus Ibarra <jesusmanuelir@gmail.com>
2020-02-01 19:10:47 -0400

    preview api

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,10 +1,18 @@
-import React from 'react';
+import React, { useState, useEffect }from 'react';
 import Navbar from '../components/Navbar';
 import Container from '../components/Container';
 import Book from '../components/Book';
 
 
 const App = () => {
+    const [book, setBook] = useState([]);
+    useEffect(() => {
+        fetch('https://www.googleapis.com/books/v1/volumes?q=search+Jesus')
+        .then(response => response.json())
+        .then(data => setBook(data));
+    },[]);
+
+    console.log(book);
     return(
         <div className="app">
         <Navbar />
