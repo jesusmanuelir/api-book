ed902ae625b0ca70bcd797bd8066fecfa079ff03
Jesus Ibarra <jesusmanuelir@gmail.com>
2020-02-01 18:52:45 -0400

    capture text search

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,14 +2,19 @@ import React from 'react';
 
 
  const Navbar = () => {
+
+    const CaptureText = () =>{
+      const res = document.getElementById("search");
+      alert(res.value);
+    }
      return (
         <nav className="navbar navbar-expand-lg navbar-expand-sm navbar-expand-md navbar navbar-dark bg-primary">
         
       
-        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+        <div className="container" id="navbarSupportedContent">
           <form className="form-inline my-2 my-lg-0 mr-auto">
-              <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"/>
-              <button className="btn btn-outline-light my-2 my-sm-0" type="submit">Search</button>
+              <input className="form-control mr-sm-2" type="input" id="search" placeholder="Search" />
+              <button className="btn btn-outline-light my-2 my-sm-0" type="button" onClick={CaptureText}>Search</button>
             </form>
           <ul className="navbar-nav ">
             <li className="nav-item">
