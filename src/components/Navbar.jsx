9ca3156b0e7c5a1fb137f81a5f742c0c1676f335
Jesus Ibarra <jesusmanuelir@gmail.com>
2020-02-01 18:05:49 -0400

    responsive book item

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,19 +7,17 @@ import React from 'react';
         
       
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul className="navbar-nav mr-auto">
-            <li className="nav-item active">
-              <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
-            </li>
+          <form className="form-inline my-2 my-lg-0 mr-auto">
+              <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"/>
+              <button className="btn btn-outline-light my-2 my-sm-0" type="submit">Search</button>
+            </form>
+          <ul className="navbar-nav ">
             <li className="nav-item">
               <a className="nav-link" href="#">Favourite</a>
             </li>
 
           </ul>
-          <form className="form-inline my-2 my-lg-0">
-            <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"/>
-            <button className="btn btn-outline-light my-2 my-sm-0" type="submit">Search</button>
-          </form>
+
         </div>
       </nav> 
       
