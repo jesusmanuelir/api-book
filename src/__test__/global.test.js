603b0221986f130579debfe89ba9d46978f3a344
Jesus Ibarra <jesusmanuelir@gmail.com>
2020-02-03 13:51:54 -0400

    Async await unit test

--- src/__test__/global.test.js
+++ src/__test__/global.test.js
@@ -1,5 +1,16 @@
-const text = "test";
+import axios from "axios";
+import "regenerator-runtime/runtime";
 
-test("check text", () => {
-  expect(text).toMatch(/test/);
-});
+
+test("Search book async await", async () => {
+    let urlGoogleAPI = `https://www.googleapis.com/books/v1/volumes`;
+    const startIndex = 1;
+    const maxResults = 12;
+    const status = 200;
+    const search = 'C++';
+    const data = await axios.get(`${urlGoogleAPI}?q=${search}&startIndex=${startIndex}&maxResults=${maxResults}`);;
+    expect(data.status).toEqual(status);
+  });
+
+
+
